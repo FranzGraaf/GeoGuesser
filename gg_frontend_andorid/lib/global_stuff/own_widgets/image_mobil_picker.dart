9ecf828a9d7de1f16985d24ba7e320f497357736
Franz Graaf <franz.graaf@kabelmail.de>
2021-07-02 15:49:35 +0200

    fixed image picker on ios

--- gg_frontend_andorid/lib/global_stuff/own_widgets/image_mobil_picker.dart
+++ gg_frontend_andorid/lib/global_stuff/own_widgets/image_mobil_picker.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:gg_frontend/global_stuff/backend_com.dart';
