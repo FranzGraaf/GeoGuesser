346073e4f27a3b1573878edf4d8959d841fb1e39
Franz Graaf <franz.graaf@kabelmail.de>
2021-10-10 22:30:28 +0200

    name changed web

--- gg_frontend/lib/main.dart
+++ gg_frontend/lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GeoGuesser',
+      title: 'GeographicGame',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
